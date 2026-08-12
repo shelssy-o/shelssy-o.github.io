3b093da5ce3d4f85bf301ffa6cb2b9440827e141
shelssy-o <ortizshelssy2@gmail.com>
2026-08-12 15:14:35 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zahit.dev',
+  site: 'https://shelssy-o.github.io',
   integrations: [mdx(), icon()],
   markdown: {
     shikiConfig: {
